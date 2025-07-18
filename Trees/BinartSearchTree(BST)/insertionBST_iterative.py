d96200cfc2ad1eb9429daece6857aa6379d33dc2
roshan13ghimire <roshan13ghimire@gmail.com>
2025-07-17 22:09:26 -0400

    added

--- Trees/BinartSearchTree(BST)/insertionBST_iterative.py
+++ Trees/BinartSearchTree(BST)/insertionBST_iterative.py
@@ -1,46 +1,51 @@
-#binary_search_tree
+#inserting_bst_using_iterative
 
 class Node:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
-
+        
 class BST:
     def __init__(self):
-        self.root = None
+        self.node = None
     
     def insert(self,data):
-        if self.root is None:
-            self.root = Node(data)
+        if self.node is None:
+            self.node =  Node(data)
             return
         
-        current = self.root
         parent = None
+        current = self.node
         while current:
             parent = current
             if data < current.data:
                 current = current.left
             else:
                 current = current.right
-        
+                
         
         if data < parent.data:
             parent.left = Node(data)
         else:
             parent.right = Node(data)
-
+        
+        
+        
+        
+        
+        
 tree = BST()
 tree.insert(8)
 tree.insert(3)
 tree.insert(10)
 tree.insert(6)
 
+print("Inorder traversal is :")
 
 def inorder(node):
     if node:
         inorder(node.left)
-        print(node.data,end = " ")
+        print(node.data, end = " ")
         inorder(node.right)
-    
-inorder(tree.root)
+inorder(tree.node)
--- Trees/BinartSearchTree(BST)/insertionBST_recursive.py
+++ Trees/BinartSearchTree(BST)/insertionBST_recursive.py
@@ -1,4 +1,4 @@
-#binary_search_tree
+#inserting_bst_using_recursion
 
 class Node:
     def __init__(self,data):
@@ -8,36 +8,30 @@ class Node:
         
 class BST:
     def __init__(self):
-        self.root = None
-    
-    def insert(self,root,data):
-        if root is None:
-            root = Node(data)
-            return root
+        self.node = None
         
-        if data < root.data:
-            root.left = self.insert(root.left, data)
-        elif data > root.data:
-            root.right = self.insert(root.right, data)
+    def insert(self,node,data):
+        if node is None:
+            return Node(data)
+        
+        if data < node.data:
+            node.left = self.insert(node.left,data)
         else:
-            pass
-    
-        return root
+            node.right = self.insert(node.right,data)
+        return node
 
-        
-        
 tree = BST()
-r = tree.root
-r = tree.insert(r,8)
-r = tree.insert(r,3)
-r = tree.insert(r,10)
-r = tree.insert(r,6)
+n = tree.node
+n = tree.insert(n,8)
+n = tree.insert(n,3)
+n = tree.insert(n,10)
+n = tree.insert(n,6)
 
+print("Inorder traversal is :")
 
 def inorder(node):
     if node:
         inorder(node.left)
-        print(node.data,end = " ")
+        print(node.data, end = " ")
         inorder(node.right)
-    
-inorder(r)
+inorder(n)
